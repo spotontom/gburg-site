701ec89b19383cf41d693972173fd08cfc69a55b
Thomas Scott <thomas@kerigan.com>
2026-02-28 09:29:39 -0600

    SOTC requested changes pt2

--- src/app/availability/page.js
+++ src/app/availability/page.js
@@ -10,16 +10,6 @@ export default function AvailabilityPage() {
       <p className={styles.text}>
         Bearadise in the Smokies is a highly sought after retreat, especially during peak season. Below you’ll find our current availability and booking options.
       </p>
-
-      <div className={styles.imageWrapper}>
-      <Image
-  src="/imgs/DSC_8397.jpg"
-  alt="Availability calendar preview"
-  width={800}
-  height={500}
-  className={styles.image}
-/>
-      </div>
       <div className="home-cta center">
           <a
             className="home-btn primary"
@@ -32,6 +22,15 @@ export default function AvailabilityPage() {
       <p className={styles.text}>
         For live calendar updates, booking inquiries, or to reserve your stay directly, use the booking tab above.
       </p>
+      <div justify-content="center" className={styles.imageWrapper}>
+      <Image
+  src="/imgs/DSC_8397.jpg"
+  alt="Availability calendar preview"
+  width={800}
+  height={500}
+  className={styles.image}
+/>
+      </div>
     </section>
   );
 }
